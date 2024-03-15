1d5e2f2eab58784a8f5f4e3868729882a95a654c
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-14 21:20:58 -0300

    aula 14/03

--- 03-busca-binaria.mjs
+++ 03-busca-binaria.mjs
@@ -1,16 +1,23 @@
+let comps
+
 function buscaBinaria(vetor, valorBusca){
+    comps = 0
     let ini = 0
     let fim = vetor.length - 1
 
     while(fim >= ini){
-        let meio = Math.floor(ini + fim)/2
+        comps++
+        let meio = Math.floor((ini + fim)/2)
         
         if(valorBusca === vetor[meio]){
+            comps++
             return meio
         }
         else if(valorBusca > vetor[meio]){
+            comps+= 2
             ini = meio + 1
         } else{
+            comps+= 2
             fim = meio - 1
         }
     }
@@ -18,4 +25,8 @@ function buscaBinaria(vetor, valorBusca){
 
 let nums = [0, 11, 22, 33, 44, 55, 66, 77, 88, 99]
 
-console.log('Posição de 33: ', buscaBinaria(nums, 33))
+//console.log('Posição de 33: ', buscaBinaria(nums, 33), 'Comparações ', comps)
+
+import {nomes} from './data/vetor-nomes.mjs'
+
+console.log("Posição de ALEXANDRE: ", buscaBinaria(nomes, "ALEXANDRE"), "comparações ", comps)
