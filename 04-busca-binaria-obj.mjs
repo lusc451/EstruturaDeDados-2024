0aafe986a8dee60fc5feac7514f71f1c35943ec1
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-15 20:20:22 -0300

    att 15/03

--- 04-busca-binaria-obj.mjs
+++ 04-busca-binaria-obj.mjs
@@ -33,5 +33,3 @@ function comparar(valorMeio){
 import { objNomes } from './data/vetor-obj-nomes.mjs'
 
 console.log('Posição do objeto em first_name === LUCAS ', buscaBinaria(objNomes, comparar), "comparações ", comps)
-
-// teste
