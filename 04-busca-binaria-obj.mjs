1b967e3efce3c5af34df69d0b210f26066590569
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-15 06:42:51 -0300

    alterações

--- 04-busca-binaria-obj.mjs
+++ 04-busca-binaria-obj.mjs
@@ -1,17 +1,23 @@
+let comps = 0
+
 function buscaBinaria(vetor, fnComp){
     let ini = 0
     let fim = vetor.length - 1
 
     while(fim >= ini){
+        comps++
         let meio = Math.floor((ini + fim)/2)
 
         switch(fnComp(vetor[meio])){
             case 0:
+                comps+= 2
                 return meio
             case 1:
+                comps+= 2
                 ini = meio + 1
                 break
             default:
+                comps+= 2
                 fim = meio -1
         }
     }
@@ -19,11 +25,11 @@ function buscaBinaria(vetor, fnComp){
 }
 
 function comparar(valorMeio){
-    if('ALEXANDRE' === valorMeio.first_name) return 0 
-    else if('ALEXANDRE' > valorMeio.first_name) return 1
+    if('LUCAS' === valorMeio.first_name) return 0 
+    else if('LUCAS' > valorMeio.first_name) return 1
     else return -1
 }
 
 import { objNomes } from './data/vetor-obj-nomes.mjs'
 
-console.log('Posição do objeto em first_name === ALEXANDRE ', buscaBinaria(objNomes, comparar))
+console.log('Posição do objeto em first_name === LUCAS ', buscaBinaria(objNomes, comparar), "comparações ", comps)
