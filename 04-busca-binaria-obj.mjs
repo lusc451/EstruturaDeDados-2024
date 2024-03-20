97bfe7e13862851b81a780028c0247f0ea23cc18
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-20 19:11:14 -0300

    teste de busca

--- 04-busca-binaria-obj.mjs
+++ 04-busca-binaria-obj.mjs
@@ -25,11 +25,11 @@ function buscaBinaria(vetor, fnComp){
 }
 
 function comparar(valorMeio){
-    if('ADRIANO' === valorMeio.first_name) return 0 
-    else if('ADRIANO' > valorMeio.first_name) return 1
+    if('EMILY' === valorMeio.first_name) return 0 
+    else if('EMILY' > valorMeio.first_name) return 1
     else return -1
 }
 
 import { objNomes } from './data/vetor-obj-nomes.mjs'
 
-console.log('Posição do objeto em first_name === ADRIANO ', buscaBinaria(objNomes, comparar), "comparações ", comps)
+console.log('Posição do objeto em first_name === EMILY ', buscaBinaria(objNomes, comparar), "comparações ", comps)
