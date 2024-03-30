8c8ba5d322f99dae0db65d0a975deff9e77636d6
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-30 20:32:12 -0300

    alterações

--- 05-bubble-sort.mjs
+++ 05-bubble-sort.mjs
@@ -1,31 +1,52 @@
 let pass, comps, trocas
 
-function bubbleSort(vetor){
+function bubbleSort(vetor) {
+
     pass = 0, comps = 0, trocas = 0
+
     let trocou
 
-    do{
+    do {
         pass++
         trocou = false
 
-        for(let i = 0; i < vetor.length -1; i++){
+        // Percurso FOR tradicional até a PENÚLTIMA posição do vetor
+        for(let i = 0; i < vetor.length - 1; i++) {
             comps++
-            if(vetor[i] > vetor[i + 1]){ //"Se o index do vetor for maior que o index subsequente"
-                [vetor[i], vetor[i + 1]] = [vetor[i + 1], vetor[i]] //Troca os valores dos index
+            if(vetor[i] > vetor[i + 1]) {
+                // Efetua a troca entre os elementos por desestruturação
+                [ vetor[i], vetor[i + 1] ] = [ vetor[i + 1], vetor[i] ]
                 trocou = true
                 trocas++
             }
         }
 
-    }while(trocou)
-
+    } while(trocou)
 }
 
-// let nums = [77, 44, 22, 33, 99, 55, 88, 0, 66, 11]
-// bubbleSort(nums)
-// console.log(nums)
+// let nums = [ 77, 44, 22, 33, 99, 55, 88, 0, 66, 11 ]
 
-import { nomes } from "./data/nomes-desord.mjs"
+// Pior caso
+// let nums = [ 99, 88, 77, 66, 55, 44, 33, 22, 11, 0 ]
+
+// Melhor caso
+//let nums = [ 0, 11, 22, 33, 44, 55, 66, 77, 88, 99 ]
+
+// console.time("Tempo de ordenação")
+// bubbleSort(nums)
+// let memoriaMB = process.memoryUsage().heapUsed / 1024 / 1024
+// console.timeEnd("Tempo de ordenação")
+// console.log(nums)
+// console.log({pass, comps, trocas, memoriaMB})
+
+/***************************************************************/
+
+import { nomes } from './data/nomes-desord.mjs'
+
+console.time('Tempo de ordenação')
 bubbleSort(nomes)
+let memoriaMB = process.memoryUsage().heapUsed / 1024 / 1024
+console.timeEnd('Tempo de ordenação')
+
 console.log(nomes)
-console.log(pass, comps, trocas)
+console.log({pass, comps, trocas, memoriaMB})
