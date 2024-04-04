7982d9fa8a1349593bcde97eb8af34ea4e388537
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-04-04 19:57:22 -0300

    Fatorial Recursivo

--- fatorial.js
+++ fatorial.js
@@ -1,17 +1,25 @@
-function calcularFatorial(n) {
-    let res = 1;
+// function calcularFatorial(n) {
+//     let res = 1;
 
-    for (let i = n; i > 1; i--) {
-        res *= i;
-    }
+//     for (let i = n; i > 1; i--) {
+//         res *= i;
+//     }
 
-    return res;
+//     return res;
+// }
+
+// console.log("O fatorial de 6 é: ", calcularFatorial(6));
+// console.log("O fatorial de 5 é: ", calcularFatorial(5));
+// console.log("O fatorial de 4 é: ", calcularFatorial(4));
+// console.log("O fatorial de 3 é: ", calcularFatorial(3));
+// console.log("O fatorial de 2 é: ", calcularFatorial(2));
+// console.log("O fatorial de 1 é: ", calcularFatorial(1));
+// console.log("O fatorial de 0 é: ", calcularFatorial(0));
+
+function fatorialRecursivo(n){
+
+    n * fatorialRecursivo(n - 1);
+
 }
 
-console.log("O fatorial de 6 é: ", calcularFatorial(6));
-console.log("O fatorial de 5 é: ", calcularFatorial(5));
-console.log("O fatorial de 4 é: ", calcularFatorial(4));
-console.log("O fatorial de 3 é: ", calcularFatorial(3));
-console.log("O fatorial de 2 é: ", calcularFatorial(2));
-console.log("O fatorial de 1 é: ", calcularFatorial(1));
-console.log("O fatorial de 0 é: ", calcularFatorial(0));
+console.log("O fatorial de 5 é: ", fatorialRecursivo(5));
