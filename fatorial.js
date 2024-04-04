5a36940f8bbf4881ee3dbb99bc98717f27d1cdfd
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-04-04 19:42:45 -0300

    Fatorial

--- fatorial.js
+++ fatorial.js
@@ -9,3 +9,8 @@ function calcularFatorial(n) {
 }
 
 console.log("O fatorial de 5 é: ", calcularFatorial(5));
+console.log("O fatorial de 4 é: ", calcularFatorial(4));
+console.log("O fatorial de 3 é: ", calcularFatorial(3));
+console.log("O fatorial de 2 é: ", calcularFatorial(2));
+console.log("O fatorial de 1 é: ", calcularFatorial(1));
+console.log("O fatorial de 0 é: ", calcularFatorial(0));
