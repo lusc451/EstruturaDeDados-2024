fca264f332187fa5e1acde2a704759aa6c332efa
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-04-04 20:17:44 -0300

    Exercicios recursividade

---
+++ 09-exercicio-recursividade.mjs
@@ -0,0 +1,9 @@
+/*
+
+1. Crie uma função recursiva que receba um número inteiro positivo N e calcule o somatório dos números de 1 a N.
+
+2. Faça uma funcão recursiva que receba um número inteiro positivo par N e imprima utilizando
+o console.log(), todos os números pares de 0 até N em ordem decrescente.
+
+*/
+
